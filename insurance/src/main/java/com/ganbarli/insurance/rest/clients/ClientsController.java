b64a08e1226eaf00e85ac59a84427b2d0ce1c7fc
faridkh <farid.q@code.edu.az>
2021-09-15 10:39:17 +0400

    Project part 1 finished

--- insurance/src/main/java/com/ganbarli/insurance/rest/clients/ClientsController.java
+++ insurance/src/main/java/com/ganbarli/insurance/rest/clients/ClientsController.java
@@ -14,7 +14,7 @@ import java.util.Set;
 @RestController
 @RequestMapping("/api")
 
-@CrossOrigin(origins = "http://localhost:80", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 public class ClientsController {
 
 
--- insurance/src/main/java/com/ganbarli/insurance/security/WebSecurityConfig.java
+++ insurance/src/main/java/com/ganbarli/insurance/security/WebSecurityConfig.java
@@ -14,17 +14,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebSecurity
 @Configuration
-@EnableWebMvc
-@EnableGlobalMethodSecurity(prePostEnabled = true,securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
